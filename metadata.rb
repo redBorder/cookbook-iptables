da2f9dab84ffd4e3e6757b746e867cf4e5418e4a
Enrique Jimenez Campos <serialsito@gmail.com>
2016-06-24 12:25:21 +0200

    Changed description

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'iptables'
 maintainer       'redborder'
 maintainer_email 'ejimenez@redborder.com'
 license          'All rights reserved'
-description      'Installs/Configures http2k'
+description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
