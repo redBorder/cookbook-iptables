b5068bb4d248f59b50c8b88b5431ae5195bfe8ac
Enrique Jimenez Campos <serialsito@gmail.com>
2016-06-29 18:04:07 +0200

    Aded boolean example in resources

--- resources/config.rb
+++ resources/config.rb
@@ -10,4 +10,4 @@ attribute :mystring, :kind_of => String, :default => "string example"
 attribute :myinteger, :kind_of => Fixnum, :default => 1
 attribute :myarray, :kind_of => Array, :default => ["val1"]
 attribute :myhash, :kind_of => Object, :default => {"val1" => "1"}
-
+attribute :myboolean, :kind_of => [TrueClass, FalseClass], :default => true
